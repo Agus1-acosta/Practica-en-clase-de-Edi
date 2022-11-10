463a9d2bdd23b4bf788f521669382a30a15089a2
Agus1-acosta <87099343+Agus1-acosta@users.noreply.github.com>
2022-11-09 21:42:22 -0300

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsService } from './products/products.service';
+import { ProductsController } from './products/products.controller';
+import { EmployeeService } from './employee/employee.service';
+import { EmployeeController } from './employee/employee.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController, EmployeeController],
+  providers: [AppService, ProductsService, EmployeeService],
 })
 export class AppModule {}
